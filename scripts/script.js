de9e55efbb0697a1d7fda07e1634f1b59976999d
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-04-12 16:51:07 -0400

    Play a single round

--- scripts/script.js
+++ scripts/script.js
@@ -3,4 +3,19 @@ function getComputerChoice() {
     return computerChoice[Math.floor(Math.random() * computerChoice.length)];
 }
 
-console.log(getComputerChoice())
+const playerSelection = prompt('Select one: Rock, Paper or Scissors.').toLowerCase();
+const computerSelection = getComputerChoice().toLowerCase();
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return `${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} ties with ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`;
+    } else if ((playerSelection === 'rock' && computerSelection === 'paper') ||
+                (playerSelection === 'paper' && computerSelection === 'scissors') ||
+                (playerSelection === 'scissors' && computerSelection === 'rock')) {
+        return `You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`;
+    } else {
+        return `You win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`;
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));

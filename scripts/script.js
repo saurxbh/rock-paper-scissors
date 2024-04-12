79eaaf71ea2149e12c30055b8b8b7f845e3ff607
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-04-12 17:13:41 -0400

    Play a 5-rounds game

--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,9 @@ function getComputerChoice() {
     return computerChoice[Math.floor(Math.random() * computerChoice.length)];
 }
 
-const playerSelection = prompt('Select one: Rock, Paper or Scissors.').toLowerCase();
-const computerSelection = getComputerChoice().toLowerCase();
-
-function playRound(playerSelection, computerSelection) {
+function playRound() {
+    let playerSelection = prompt('Select one: Rock, Paper or Scissors.').toLowerCase();
+    let computerSelection = getComputerChoice().toLowerCase();
     if (playerSelection === computerSelection) {
         return `${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} ties with ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`;
     } else if ((playerSelection === 'rock' && computerSelection === 'paper') ||
@@ -18,4 +17,36 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-console.log(playRound(playerSelection, computerSelection));
+// console.log(playRound(playerSelection, computerSelection));
+
+let ties = 0;
+let playerWins = 0;
+let computerWins = 0;
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        console.log(i);
+        let roundResult = playRound();
+        console.log(roundResult);
+        determineWinner(roundResult);
+    }
+    if (playerWins === computerWins) {
+        console.log(`It's a tie, both you and the computer won ${playerWins} round(s).`);
+    } else if (playerWins > computerWins) {
+        console.log(`You win! You won ${playerWins} round(s) and the computer won ${computerWins} round(s).`)
+    } else {
+        console.log(`You lose! The computer won ${computerWins} round(s) and you won ${playerWins} round(s).`)
+    }
+}
+
+function determineWinner(resultString) {
+    if (resultString.includes('ties')) {
+        ties++;
+    } else if (resultString.includes('win')) {
+        playerWins++;
+    } else if (resultString.includes('lose')) {
+        computerWins++;
+    }
+}
+
+playGame()

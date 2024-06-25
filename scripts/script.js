5684df5e1ca18d2a63096528292fb5401ba9a966
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-06-25 10:23:47 -0400

    Play a round and display the result

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,47 @@
+let ties = 0;
+let playerWins = 0;
+let computerWins = 0;
+const score = document.querySelector('#score');
+
+const buttons = document.querySelectorAll('input');
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        playRound(button.value);
+    })
+})
+
 function getComputerChoice() {
     let computerChoice = ['Rock', 'Paper', 'Scissors'];
     return computerChoice[Math.floor(Math.random() * computerChoice.length)];
 }
 
-function playRound() {
+function playRound(playerSelection) {
     // let playerSelection = prompt('Select one: Rock, Paper or Scissors.').toLowerCase();
+    playerSelection = playerSelection.toLowerCase();
+
     let computerSelection = getComputerChoice().toLowerCase();
+    const computer_selection = document.querySelector('#computer_selection');
+    computer_selection.textContent = `Computer Selection: ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`;
+
+    const result = document.querySelector('#result');
     if (playerSelection === computerSelection) {
-        return `${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} ties with ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`;
+        result.textContent = `${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} ties with ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`;
+        score.textContent = `Player Wins: ${playerWins}, Computer Wins: ${computerWins}, Ties: ${ties + 1}`;
+        return
     } else if ((playerSelection === 'rock' && computerSelection === 'paper') ||
                 (playerSelection === 'paper' && computerSelection === 'scissors') ||
                 (playerSelection === 'scissors' && computerSelection === 'rock')) {
-        return `You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`;
+        result.textContent = `You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`;
+        score.textContent = `Player Wins: ${playerWins}, Computer Wins: ${computerWins + 1}, Ties: ${ties}`;
+        return
     } else {
-        return `You win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`;
+        result.textContent = `You win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`;
+        score.textContent = `Player Wins: ${playerWins + 1}, Computer Wins: ${computerWins}, Ties: ${ties}`;
+        return
     }
 }
 
-const button = document.querySelector('button');
-button.addEventListener('onclick', playRound);
-
 // console.log(playRound(playerSelection, computerSelection));
-
-let ties = 0;
-let playerWins = 0;
-let computerWins = 0;
 
 function playGame() {
     for (let i = 0; i < 5; i++) {
